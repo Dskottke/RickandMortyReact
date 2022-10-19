f2e290b0861dca0644559406e2e006a3785d1e76
davidskottke <davidskottke@googlemail.com>
2022-10-19 16:41:29 +0200

    add footer

---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Footer.css"
+
+//rsf macht automatisch eine maske für Funktionen
+function Footer() {
+    return (
+        <footer>
+            <h6>© Hier waren die <span className="span">Besten</span> am Werk</h6>
+        </footer>
+    );
+}
+
+export default Footer;
