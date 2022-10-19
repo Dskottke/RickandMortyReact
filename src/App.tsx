276f4892a4eca886816b9b24163209cfc03caeb0
davidskottke <davidskottke@googlemail.com>
2022-10-19 17:20:31 +0200

    Changes by marc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Charactergalery from "./Charactergalery";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return <>
 
     <Header></Header>
     <Charactergalery></Charactergalery>
+    <Footer />
 
   </>
 
