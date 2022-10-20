a60cb824f3994efcd229148bbc41acc98bb7e318
davidskottke <davidskottke@googlemail.com>
2022-10-20 17:20:53 +0200

    add filter

--- src/Charactergalery.tsx
+++ src/Charactergalery.tsx
@@ -4,6 +4,7 @@ import "./Charactergalery.css"
 import {nanoid} from "nanoid";
 import axios from "axios";
 import {Character} from "./CharacterModel"
+import Inputfield from "./Inputfield";
 export default function Charactergalery() {
 
 //https://rickandmortyapi.com/api/character?page=2"
@@ -21,28 +22,29 @@ export default function Charactergalery() {
                 setCharacter(data.results)
             })},[])
 
-
-
-
-    /*   const characterResponse = () => axios.get("https://rickandmortyapi.com/api/character?page=2")
-        .then((response) => {
-            return response.data
-        })
-        .catch((error) => console.log("error" + error))
-        .then((data) => {
-            setCharacter(data.results)
-        })  */
-
+    const [name,setName]=useState<string>("")
+    const filteredCharacter = characterList.filter((elem) => elem.name.includes(name))
 
 
 
 return (
-    <main> {characterList.map((current) => {
+
+    <main>
+        <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+        />
+
+
+
+        {filteredCharacter.map((current) => {
         return <Charactercard
             name={current.name}
             image={current.image}
             origin={current.origin.name}
             key={nanoid()}/>
+
     })}
     </main>
 
