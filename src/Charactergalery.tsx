26dff211f9cc67483f6e8d080142cae586a5c0ce
davidskottke <davidskottke@googlemail.com>
2022-10-20 19:12:59 +0200

    add paging

--- src/Charactergalery.tsx
+++ src/Charactergalery.tsx
@@ -4,49 +4,58 @@ import "./Charactergalery.css"
 import {nanoid} from "nanoid";
 import axios from "axios";
 import {Character} from "./CharacterModel"
-import Inputfield from "./Inputfield";
+
+
 export default function Charactergalery() {
 
 //https://rickandmortyapi.com/api/character?page=2"
 
-
-
     const [characterList, setCharacter] = useState<Character[]>([])
+    const [side, setside] = useState<number>(0)
+    const increase = () => {
+        setside(side + 1)
+    }
 
-        useEffect(() =>{ axios.get("https://rickandmortyapi.com/api/character?page=2")
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/character?page=" + side.toString())
             .then((response) => {
                 return response.data
             })
             .catch((error) => console.log("error" + error))
             .then((data) => {
                 setCharacter(data.results)
-            })},[])
+            })
+    }, [side])
 
-    const [name,setName]=useState<string>("")
+    const [name, setName] = useState<string>("")
     const filteredCharacter = characterList.filter((elem) => elem.name.includes(name))
 
 
+    return (
 
-return (
+        <main>
 
-    <main>
-        <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-        />
-
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            ></input>
 
 
-        {filteredCharacter.map((current) => {
-        return <Charactercard
-            name={current.name}
-            image={current.image}
-            origin={current.origin.name}
-            key={nanoid()}/>
+            {filteredCharacter.map((current) => {
+                return <Charactercard
+                    name={current.name}
+                    image={current.image}
+                    origin={current.origin.name}
+                    key={nanoid()}/>
 
-    })}
-    </main>
 
-)
+            })}
+
+            <button onClick={increase}>next page</button>
+
+        </main>
+
+    )
 }
--- src/Inputfield.tsx
+++ src/Inputfield.tsx
@@ -1,27 +1,17 @@
 import React, {useState} from 'react';
 
-function Inputfield(props:{name:string}) {
+function Inputfield() {
 
-    const [name, setName] = useState("");
+
 
     const handleSubmit = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
         alert(`The name you entered was: ${name}`)
     }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>Enter your name:
-                <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-            </label>
-            <input type="submit" />
-        </form>
-    )
+
 
 }
+
 
 export default Inputfield;
