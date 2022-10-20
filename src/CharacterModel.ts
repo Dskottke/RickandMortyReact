b3495969dc053d35bf8a62b9f5abb2f34a3097de
davidskottke <davidskottke@googlemail.com>
2022-10-20 16:20:24 +0200

    add axios

---
+++ src/CharacterModel.ts
@@ -0,0 +1,21 @@
+
+export type Character = {
+       name: string;
+      status: string,
+    image: string;
+     gender: string;
+    species: string;
+    created: string;
+    origin: {
+        name: string;
+        url: string
+    };
+    location: {
+        name: string;
+        url: string
+    };
+    episode: string[];
+    id: number;
+    type: string;
+    url: string;
+}
